fbe406b97ef6223e78caabd49a91531904c51b32
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-12-09 19:16:08 +0500

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,7 @@ import {
 export default function Home() {
   return (
     <div className='bg-[#f6f7f9]  min-h-screen p-4 sm:p-6 lg:p-20 flex flex-col gap-10 font-'>
-  
-      </section> */}
+
       <div className="first w-full flex flex-wrap sm:flex-nowrap gap-4 sm:gap-8 justify-center">
           {/* Left Card */}
           <div
