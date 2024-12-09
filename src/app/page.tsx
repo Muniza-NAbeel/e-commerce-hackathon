094b12a3909b7e8c44e5fd977fe81e8bd2760d2b
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-12-09 19:15:22 +0500

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,21 +12,7 @@ import {
 export default function Home() {
   return (
     <div className='bg-[#f6f7f9]  min-h-screen p-4 sm:p-6 lg:p-20 flex flex-col gap-10 font-'>
-      {/* <section className='first w-full flex flex-wrap sm:flex-nowrap gap-4 sm:gap-8 justify-center'>
-        <Image
-          src={"/Ads 1.png"}
-          alt=''
-          width={640}
-          height={360}
-          className='max-w-full'
-        />
-        <Image
-          src={"/Ads 2.png"}
-          alt=''
-          width={640}
-          height={360}
-          className='max-w-full'
-        />
+  
       </section> */}
       <div className="first w-full flex flex-wrap sm:flex-nowrap gap-4 sm:gap-8 justify-center">
           {/* Left Card */}
