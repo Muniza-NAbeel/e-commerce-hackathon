c688692148be65c32c714136f87fc50ad9589b1b
Muniza .. <159540004+Muniza-NAbeel@users.noreply.github.com>
2024-12-09 13:03:30 +0500

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['example.com'],
+  },
+};
 
 export default nextConfig;
